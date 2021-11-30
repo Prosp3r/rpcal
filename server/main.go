cec78800051272abf00e33064f2308d756237963
Prosper <sirpros@gmail.com>
2021-11-30 05:05:05 +0100

    Some updates

--- server/main.go
+++ server/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	pb "github.com/Prosp3r/rpcal/proto"
+)
+
+const (
+	port = ":50051"
+)
+
+type repository interface {
+	Create(*pb.Calendar) ([]*pb.Calendar, error)
+}
+
 func main() {
 
 }
