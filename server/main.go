5211f3b53f54f5508fb175643a8070fdfd8e3276
Prosper <sirpros@gmail.com>
2021-11-30 05:48:51 +0100

    Fixed wrong proto definition

--- server/main.go
+++ server/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"sync"
+
 	pb "github.com/Prosp3r/rpcal/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 const (
@@ -10,6 +15,32 @@ const (
 
 type repository interface {
 	Create(*pb.Calendar) ([]*pb.Calendar, error)
+	GetAll() []*pb.Calendar
+}
+
+type Repository struct {
+	mu sync.RWMutex
+	calendars []*pb.Calendar
+}
+
+func (repo *Repository) Create(calendar *pb.Calendar) (*pb.Calendar, error){
+
+	repo.mu.Lock()
+	repo.calendars = append(repo.calendars, calendar)
+	repo.mu.Unlock()
+	return calendar, nil
+}
+
+type service struct {
+	repo repository
+}
+
+func (s *service) CreateCalendar(ctx context.Context, req *pb.Calendar) (*pb.Response, error){
+	calendar, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.Response{Created: true, Calendar: calendar}, nil
 }
 
 func main() {
